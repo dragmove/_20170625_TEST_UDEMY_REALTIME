f69fe6af789da830cc0b902d29c3cdb748d44a8f
dragmove <dragmove@naver.com>
2017-06-25 19:57:02 +0900

    work server webpack setting

---
+++ src/server/server.js
@@ -0,0 +1,11 @@
+import 'source-map-support/register';
+
+import {blegh} from 'shared/test';
+// import _ from 'lodash';
+
+blegh();
+console.log('from server.');
+// throw new Error('blegh');
+
+// const arr = [1, 2, 3, 4, 5];
+// console.log(_.filter(arr, a => a < 5));

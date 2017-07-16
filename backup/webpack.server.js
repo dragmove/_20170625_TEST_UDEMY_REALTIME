381a4503065bb1488f5e556d3e1d5acc76deab66
dragmove <dragmove@naver.com>
2017-07-17 02:12:22 +0900

    Update : work 10.Server Webpack: Configuration Final

---
+++ backup/webpack.server.js
@@ -0,0 +1,70 @@
+const webpack = require('webpack'),
+  path = require('path');
+
+function createConfig(isDebug) {
+  let plugins = [];
+
+  if (!isDebug) {
+    let uglifyJSPlugin = new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        drop_console: false,
+        warnings: false
+      },
+      sourceMap: true
+    });
+
+    plugins.push(uglifyJSPlugin);
+  }
+
+  return {
+    context: __dirname,
+
+    target: 'node',
+
+    devtool: 'source-map', // https://webpack.js.org/configuration/devtool/
+
+    entry: {
+      'server': './src/server/server.js'
+    },
+
+    output: {
+      libraryTarget: 'umd',
+      path: path.join(__dirname, 'build'),
+      filename: '[name].js'
+    },
+
+    resolve: {
+      alias: {
+        shared: path.join(__dirname, 'src', 'shared')
+      }
+    },
+
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        }, {
+          test: /\.js$/,
+          loader: 'eslint-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+
+    externals: {
+      lodash: {
+        root: '_',
+        commonjs: 'lodash',
+        commonjs2: 'lodash',
+        amd: "lodash"
+      }
+    },
+
+    plugins: plugins
+  };
+}
+
+module.exports = createConfig(true);
+module.exports.create = createConfig;
--- webpack.server.js
+++ webpack.server.js
@@ -1,10 +1,24 @@
-const webpack = require('webpack'),
-  path = require('path');
+var path = require("path"),
+	fs = require("fs"),
+	webpack = require("webpack");
+	
+const nodeModules = fs.readdirSync("./node_modules").filter(d => d != ".bin");
+function ignoreNodeModules(context, request, callback) {
+	if (request[0] == ".")
+		return callback();
+		
+	const module = request.split("/")[0];
+	if (nodeModules.indexOf(module) !== -1) {
+		return callback(null, "commonjs " + request);
+	}
+	
+	return callback();
+}
 
 function createConfig(isDebug) {
-  let plugins = [];
-
-  if (!isDebug) {
+	const plugins = [];
+	
+	if (!isDebug) {
     let uglifyJSPlugin = new webpack.optimize.UglifyJsPlugin({
       compress: {
         drop_console: false,
@@ -14,40 +28,34 @@ function createConfig(isDebug) {
     });
 
     plugins.push(uglifyJSPlugin);
-  }
-
-  return {
+	}
+	
+	// ---------------------
+	// WEBPACK CONFIG
+	return {
     context: __dirname,
-    target: 'node',
-    devtool: 'source-map', // https://webpack.js.org/configuration/devtool/
+		target: "node",
+		devtool: "source-map",
     entry: {
       'server': './src/server/server.js'
     },
-    output: {
+		output: {
       libraryTarget: 'umd',
-      path: path.join(__dirname, 'build'),
-      filename: '[name].js'
-    },
-
-    resolve: {
-      alias: {
-        shared: path.join(__dirname, 'src', 'shared')
-      }
-    },
-
+      path: path.join(__dirname, "build"),
+			filename: "[name].js"
+		},
+		resolve: {
+			alias: {
+				shared: path.join(__dirname, "src", "shared")
+			}
+		},
     module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/
-        }, {
-          test: /\.js$/,
-          loader: 'eslint-loader',
-          exclude: /node_modules/
-        }
-      ]
-    },
+			rules: [
+				{ test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
+				{ test: /\.js$/, loader: "eslint-loader", exclude: /node_modules/ }
+			]
+		},
+		externals: [ignoreNodeModules],
 
     externals: {
       lodash: {
@@ -58,8 +66,9 @@ function createConfig(isDebug) {
       }
     },
 
-    plugins: plugins
-  };
+		plugins: plugins
+	};
+	// ---------------------
 }
 
 module.exports = createConfig(true);
